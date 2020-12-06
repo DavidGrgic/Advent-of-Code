6078d222958542abc6c36198f5b64852c158023b
david_grgic <david.grgic@gen-i.si>
2020-12-06 07:23:56 +0100

    day 6

---
+++ 2020/06/naloga6.py
@@ -0,0 +1,32 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd
+    
+def main():
+    data = ''
+    with open('data.txt', 'r') as file:
+        for c, l in enumerate(file):
+            data += l
+
+    data1 = data.replace('\n\n', '#').replace('\n', '')
+    data1 = eval('["' +  data1.replace('#', '","') + '"]')
+    dataset = [{j for j in i} for i in data1]
+    dataset = [len(i) for i in dataset]
+    print(sum(dataset))
+    
+    data2 = data.replace('\n\n', '")],[("').replace('\n', '"),("')
+    data2 = eval('[("' + data2 + '")]')
+    dataset = []
+    for i in data2:
+        dta = {i for i in 'abcdefghijklmnopqrstuvwxyz'}
+        for j in i:
+            dta &= {k for k in j}
+        dataset += [dta]
+    dataset = [len(i) for i in dataset]
+    print(sum(dataset))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2020/score.py
@@ -0,0 +1,38 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd
+import os, json
+    
+def main():
+    _json = os.path.join(os.path.dirname(__file__), 'score.json')
+    if not os.path.isfile(_json):
+        return
+    with open(_json) as f:
+        sco = json.load(f)
+    
+    score = []
+    for user_id, data in sco.get('members').items():
+        name = data.get('name')
+        score += (lambda N = name, D = data: [(int(user_id), N, int(d), int(p), int(w.get('get_star_ts'))) for d, v in D.get('completion_day_level').items() for p, w in v.items()])()
+    score = pd.DataFrame(score, columns = ['ID', 'Name', 'Day', 'Part', 'Time'])
+    score['UTC'] = score['Time'].astype('datetime64[s]')
+    score['LT'] = pd.DatetimeIndex(score['UTC']).tz_localize('UTC').tz_convert('CET').tz_localize(None)
+    score['LTstart'] = [pd.Timestamp('2020-12-01') + pd.Timedelta(days = v['Day']-1) + pd.Timedelta(hours = 6) for i, v in score.iterrows()]
+    score['Minutes'] = (score['LT'] - score['LTstart']).astype('timedelta64[s]')/60
+
+    score_id = score.groupby('ID')['Name'].first()
+
+    # Day/Part statistic
+    minutes = score.loc[(score['Day'] == 1) & (score['Part'] == 2), ['Name', 'Day', 'LT', 'Minutes']].sort_values('Minutes')
+
+    # Name statistic 
+    names = score.loc[score['Name'] == 'Blaž Peterlin'].sort_values(['Day', 'Part'])
+    
+    # AggregateByNames statistic
+    score.loc[score['Part'] == 2].groupby('ID')[['Minutes']].mean().sort_values(['Minutes']).join(score_id)
+    
+
+if __name__ == '__main__':
+    main()
