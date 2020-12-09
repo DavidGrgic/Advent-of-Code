7f8ecf7c09f0b3900e0f5254012b27946002f8e1
david_grgic <david.grgic@gen-i.si>
2020-12-09 08:31:07 +0100

    day 9

---
+++ 2020/09/naloga9.py
@@ -0,0 +1,47 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd
+
+def main():
+    
+    # Read
+    data = pd.read_csv('data.csv', header = None).iloc[:,0].astype(int)
+
+    obseg = 25
+    # Part 1
+    i = obseg
+    while True:
+        ssub = data.iloc[i-obseg:i].values
+        vsota = data.iloc[i]
+        sub = ssub.reshape(-1,1) + ssub.reshape(1,-1)
+        res = set()
+        j = 0
+        for su in sub:
+            j += 1
+            res |= set(su[j:])
+        if vsota in res:
+            i += 1
+        else:
+            break
+    print(vsota)
+    iskana = int(vsota)
+
+    # Part 2
+    ok = False
+    for j in range(2,1000):
+#        print(j)
+        for i in range(j, 1000):
+#            print(i)
+            vsota = int(data.iloc[i-j:i].sum()) # int(data.iloc[i-j]) + int(data.iloc[i])
+            if iskana == vsota:
+                ok = True
+                break
+        if ok:
+            break
+    sub = data.iloc[i-j:i]
+    print(sub.min()+ sub.max())
+
+if __name__ == '__main__':
+    main()
---
+++ 2020/naloga00.py
@@ -0,0 +1,27 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd
+
+def main():
+    
+    # Read
+    data = pd.read_csv('data.csv', header = None).iloc[:,0].astype(int)
+    # data = []
+    # with open('data.txt', 'r') as file:
+    #     for c, ln in enumerate(file):
+    #         ins, val = ln.split(' ')
+    #         data += [(ins, eval(val))]
+
+    # Part 1
+
+    print(data)
+
+
+    # Part 2
+
+    print(data)
+
+if __name__ == '__main__':
+    main()
