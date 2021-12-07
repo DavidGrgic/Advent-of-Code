96568fcc00224e8869f4db05260f9779c6577175
david_grgic <david.grgic@gen-i.si>
2021-12-07 07:55:39 +0100

    Day7

--- 2021/06/naloga06_performance_chk.py
+++ 2021/06/naloga06_performance_chk.py
@@ -3,7 +3,7 @@
 @author: David Grgic
 """
 import pandas as pd, numpy as np
-import os, sys
+import os, sys, time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
 import mat
 
@@ -16,28 +16,35 @@ def main():
             data = ln.split(',')
     data = np.array([int(i) for i in data])
 
-
+    dni = 192
     # Part 1
     dat = data.copy()
-    if True:
-        for i in range(80):
-            nov = dat == 0
-            dat[nov] = 7
-            dat = np.append(dat,9*np.ones(nov.sum()).astype(int))
-            dat -= 1
-        print(f"A1: {dat.shape[0]}")
+    cas1 = [time.time()]
+    for i in range(dni):
+        nov = dat == 0
+        dat[nov] = 7
+        dat = np.append(dat,9*np.ones(nov.sum()).astype(int))
+        dat -= 1
+        cas1 += [time.time()]
+    print(f"A1: {dat.shape[0]}")
 
 
     # Part 2
     dat = pd.DataFrame([(i,0) for i in data], columns = ['D', 'C']).groupby('D')['C'].count().reindex(range(9), fill_value = 0)
-    for i in range(256):
+    cas2 =[time.time()]
+    for i in range(dni):
         nov = dat.loc[0]
         dat.loc[7] += nov
         dat.loc[9] = nov
         dat.iloc[:-1] = dat.iloc[1:].values
         dat.loc[9] = 0
+        cas2 += [time.time()]
     print(f"A2: {dat.sum()}")
 
+    cas = pd.DataFrame([cas1, cas2], index = [1,2], columns = range(dni+1)).T
+    for i in cas.columns:
+        cas.loc[:,i] -= cas.loc[0,i]
+    cas.iloc[1:].round(3).to_csv(__file__.replace('py', 'csv'), sep = ';', decimal = ',', header = False)
 
 if __name__ == '__main__':
     main()
---
+++ 2021/07/naloga07.py
@@ -0,0 +1,45 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+import mat
+
+def main():
+
+    # Read
+    with open('data.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            if ln == '': # Nov blok podatkov
+                pass
+            da = ln.split(',')
+    data = np.array([int(i) for i in da])
+
+    # Part 1
+    if True:
+        dat = data * np.ones((data.max()+1,1)).astype(int)
+        for i in range(dat.shape[0]):
+            dat[i,:] = abs(dat[i,:] - i-1)
+        dat = dat.sum(axis = 1)
+        res = dat[np.where(dat == dat.min())[0][0]]
+        print(f"A1: {res}")
+          
+    
+    # Part 2
+    dat = data * np.ones((data.max()+1,1)).astype(int)
+    cost = pd.Series([i for i in range(dat.shape[0]+1)]).cumsum().to_dict()
+    for i in range(dat.shape[0]):
+        mov = abs(dat[i,:] - i-1)
+        for j in range(mov.shape[0]):
+            mov[j] = cost[mov[j]]
+        dat[i,:] = mov
+    dat = dat.sum(axis = 1)
+    res = dat[np.where(dat == dat.min())[0][0]]
+    print(f"A2: {res}")
+
+
+if __name__ == '__main__':
+    main()
