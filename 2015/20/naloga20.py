2162d7c0b3898257949898a9ceb4f9e9a65ce6ab
david_grgic <david.grgic@gen-i.si>
2022-01-06 08:34:08 +0100

    y2015d20

--- 2015/20/naloga20.py
+++ 2015/20/naloga20.py
@@ -12,28 +12,32 @@ def main():
     # Read
     data = 33100000
 
+
     def factors(n):
         # https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
         return set(x for tup in ([i, n//i] for i in range(1, int(n**0.5)+1) if n % i == 0) for x in tup)
 
 
-    # Part 1
-    if True:
-        house = 10**5
+    def obiski(per_house, per_elve = None, house = 0):
         presents = 0
         while presents < data:
-#            if (house % 10**4) == 0: print(house)
             house += 1
             presents = 0
-            for elv in factors(house):
-                if (house % elv) == 0:
-                    presents += 10 * elv
+            for elve in factors(house):
+                if per_elve is None or (house / elve) <= per_elve:
+                    presents += per_house * elve
+        return house
+
+
+    # Part 1
+    if True:
+        house = obiski(10)
         print(f"A1: {house}")
           
     
     # Part 2
-
-    print(f"A2: {0}")
+    house = obiski(11, 50)
+    print(f"A2: {house}")
 
 
 if __name__ == '__main__':
