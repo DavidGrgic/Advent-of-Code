97903ac5a068cb177b8a1301fdc035c039a5b8fb
DavidGrgic <david.grgic@gmail.com>
2022-11-19 22:39:58 +0100

    Ogrevanje

---
+++ 2019/01/naloga01.py
@@ -0,0 +1,42 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+import mat
+
+def main():
+
+    # Read
+    data = []
+    with open('data.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            data += [int(ln)]
+
+
+    fuel = lambda x: sum(i//3 - 2 for i in x)
+    # Part 1
+    if True:
+         print(f"A1: {fuel(data)}")
+          
+    
+    # Part 2
+    mass = []
+    for i in data:
+        ma = (i,)
+        while True:
+            m = max(0, fuel(ma)-sum(ma[1:]))
+            if m > 0:
+                ma += (m,)
+            else:
+                break
+        mass += [ma]
+    fu = [sum(i[1:]) for i in mass]
+    print(f"A2: {sum(fu)}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2019/naloga00.py
@@ -0,0 +1,35 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+import mat
+
+def main():
+
+    # Read
+    data = []
+    with open('tdata.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            if ln == '': # Nov blok podatkov
+                pass
+            da = ln.split(',')
+            data += [da]
+
+
+    # Part 1
+    if True:
+
+        print(f"A1: {0}")
+          
+    
+    # Part 2
+
+    print(f"A2: {0}")
+
+
+if __name__ == '__main__':
+    main()
