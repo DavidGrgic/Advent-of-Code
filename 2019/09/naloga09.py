1f1db3cb83f615d3305790dc0ea882c3f310ef5f
DavidGrgic <david.grgic@gmail.com>
2022-11-26 23:34:36 +0100

    2019-09

---
+++ 2019/09/naloga09.py
@@ -0,0 +1,88 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+from collections import Counter
+from itertools import permutations, combinations, product
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+import mat
+_img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]))
+
+def main():
+
+    # Read
+    with open('data.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            data = {i: int(v) for i, v in enumerate(ln.split(','))}
+
+
+    def intcode(dat, _input = [], pos = 0, base = 0):
+        _output = []
+        address = lambda p, m: p+base if m == 2 else p
+        value = lambda p, m: dat.get(address(dat[p],m), 0) if m in {0,2} else dat.get(p, 0)
+        while True:
+            ins = str(dat[pos])
+            ins = (5-len(ins))*'0' + ins
+            mod = [int(i) for i in ins[-3::-1]]
+            ins = int(ins[-2:])
+            if ins == 99:
+                break
+            if ins == 1:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = value(pos+1, mod[0]) + value(pos+2, mod[1])
+                pos += 4
+            elif ins == 2:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = value(pos+1, mod[0]) * value(pos+2, mod[1])
+                pos += 4
+            elif ins == 3:
+                assert mod[0] in {0,2}
+                assert len(_input) > 0
+                dat[address(dat[pos+1], mod[0])] = _input[0]
+                _input = _input[1:]
+                pos += 2
+            elif ins == 4:
+                _output.append(value(pos+1, mod[0]))
+                pos += 2
+            elif ins == 5:
+                if value(pos+1, mod[0]) != 0:
+                    pos = value(pos+2, mod[1])
+                else:
+                    pos += 3
+            elif ins == 6:
+                if value(pos+1, mod[0]) == 0:
+                    pos = value(pos+2, mod[1])
+                else:
+                    pos += 3
+            elif ins == 7:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = 1 if value(pos+1, mod[0]) < value(pos+2, mod[1]) else 0
+                pos += 4
+            elif ins == 8:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = 1 if value(pos+1, mod[0]) == value(pos+2, mod[1]) else 0
+                pos += 4
+            elif ins == 9:
+                base += value(pos+1, mod[0])
+                pos += 2
+            else:
+                raise AssertionError
+        return dat, _output, -1, base
+
+
+    # Part 1
+    if True:
+        p1 = intcode(data.copy(), [1])
+        print(f"A1: {p1[1][-1]}")
+          
+    
+    # Part 2
+    p2 = intcode(data.copy(), [2])
+    print(f"A2: {p2[1][-1]}")
+
+
+if __name__ == '__main__':
+    main()
