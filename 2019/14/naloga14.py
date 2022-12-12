fa1469dd1b9a706870a98eb600f91c71196b7f3d
DavidGrgic <david.grgic@gmail.com>
2022-12-12 07:02:33 +0100

    2022-12a

--- 2019/14/naloga14.py
+++ 2019/14/naloga14.py
@@ -15,7 +15,7 @@ def main():
 
     # Read
     data = {}
-    with open('t1.txt', 'r') as file:
+    with open('d.txt', 'r') as file:
         for c, ln in enumerate(file):
             ln = ln.replace('\n', '')
             da = ln.split(' => ')
@@ -25,28 +25,50 @@ def main():
 
     cup = lambda x, y: x // y + (x % y > 0)
 
-    def cons(num, chm):
+    def pth(chm):
         nn, dep = data[chm]
-        mul = cup(num, nn)
-        ostanek.update({chm: ostanek.get(chm, 0) + mul * nn - num})
-
-        ore = 0
+        ore = set()
         for i in dep:
-            poraba = mul * i[0]
-            reciklacija = min(poraba, ostanek.get(i[1],0))
-            ostanek.update({i[1]: ostanek.get(i[1], 0) - reciklacija})
-            potreba = poraba - reciklacija
             if i[1] == 'ORE':
-                ore += potreba
+                ore |= {((chm,nn,i[0]),(i[1],1,1))}
             else:
-                ore += cons(*i)
+                ore |= {((chm,nn,i[0]),)+j for j in  pth(i[1])}
         return ore
 
-    ostanek = {}
+    # def cons(num, chm):
+    #     nn, dep = data[chm]
+    #     mul = cup(num, nn)
+    #     ostanek.update({chm: ostanek.get(chm, 0) + mul * nn - num})
+
+    #     ore = 0
+    #     for i in dep:
+    #         poraba = mul * i[0]
+    #         reciklacija = min(poraba, ostanek.get(i[1],0))
+    #         ostanek.update({i[1]: ostanek.get(i[1], 0) - reciklacija})
+    #         potreba = poraba - reciklacija
+    #         if i[1] == 'ORE':
+    #             ore += potreba
+    #         else:
+    #             ore += cons(*i)
+    #     return ore
+    # ostanek = {}
+
     # Part 1
     if True:
-        p1 = cons(1, 'FUEL')
-        print(f"A1: {p1}")
+        red = ['FUEL', 'ORE']
+        delo = pth(red[0])
+        for pot in {tuple(j[0] for j in i) for i in delo}:
+            if pot == tuple(red):
+                continue
+            j = 0
+            for r in pot:
+                if r == red[j]:
+                    pass
+                elif not r in red[j:]:
+                    red.insert(j, r)
+                j += 1
+        
+        print(f"A1: {0}")
           
     
     # Part 2
---
+++ 2022/12/naloga12.py
@@ -0,0 +1,62 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+import math, copy
+from collections import Counter
+from fractions import Fraction
+from itertools import permutations, combinations, product
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+_img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]))
+
+def main():
+    # Read
+    data = []
+    with open('data.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            if 'S' in ln:
+                start = (c, ln.index('S'))
+            if 'E' in ln:
+                end = (c, ln.index('E'))
+            ln = ln.replace('S','a').replace('E','z')
+            da = [ord(i) for i in ln]
+            data += [da]
+    data = np.array(data)
+    data = (data - data.min()).astype(int)
+
+    plus = lambda x, y: (x[0]+y[0],x[1]+y[1])
+    xy = data.shape
+
+    def pot(p):
+#        print(p)
+        res = set()
+        for d in [(1,0),(-1,0),(0,1),(0,-1)]:
+#            print(d)
+            n = plus(p[-1],d)
+            if n in p:
+                continue
+            if not (0 <= n[0] < xy[0] and 0 <= n[1] < xy[1]):
+                continue
+            if n == end and data[n] <= data[p[-1]] + 1:
+                res = {p + (n,)}
+            if data[n] <= data[p[-1]] + 1:
+                ppp = pot(p+(n,))
+                res |= ppp
+        return res
+        
+
+    # Part 1
+    if True:
+#        dat=copy.deepcopy(data)
+        poti = pot((start,))
+        poti = [p for p in poti if p[-1] == end]
+        print(f"A1: {min([len(p) for p in poti])-1}")
+
+    # Part 2
+    print(f"A2: {0}")
+
+if __name__ == '__main__':
+    main()
