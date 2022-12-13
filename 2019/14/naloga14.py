176916aa6006bc634272c45186ae29399ee18703
DavidGrgic <david.grgic@gmail.com>
2022-12-13 22:29:56 +0100

    2019-14

--- 2019/14/naloga14.py
+++ 2019/14/naloga14.py
@@ -30,22 +30,33 @@ def main():
         ore = set()
         for i in dep:
             if i[1] == 'ORE':
-                ore |= {((chm,nn,i[0]),(i[1],1,1))}
+                ore |= {(chm, i[1])}
             else:
-                ore |= {((chm,nn,i[0]),)+j for j in  pth(i[1])}
+                ore |= {(chm,)+j for j in  pth(i[1])}
         return ore
+
+    def fuel(enot):
+        material = {'FUEL': enot}
+        for r in red:
+            if r == 'ORE':
+                break
+            num = material[r]
+            nn, dep = data[r]
+            mul = cup(num, nn)
+            material.update({r: mul*nn - num})
+            for i in dep:
+                material.update({i[1]: material.get(i[1],0) + mul * i[0]})
+        return material['ORE']
 
     # Part 1
     if True:
-        red = ['FUEL', 'ORE']
-        delo = pth(red[0])
+        delo = pth('FUEL')
 
-        surovine = {j[0] for i in delo for j in i}
+        surovine = {j for i in delo for j in i}
         depend = {}
         for s in surovine:
             dep = set()
-            for dd in delo:
-                d = [d[0] for d in dd]
+            for d in delo:
                 if s in d:
                     dep |= set(d[d.index(s)+1:])
             depend.update({s: dep})
@@ -53,22 +64,20 @@ def main():
         red = []
         while len(red) < len(depend):
             red = [k for k, v in depend.items() if all(i in red for i in v) and k not in red] + red
-        material = {'FUEL': 1}
-        for r in red:
-            if r == 'ORE':
-                break
-            num = material[r]
-            nn, dep = data[r]
-            mul = cup(num, nn)
-            material.update({r: material.get(r,0) - num})
-            for i in dep:
-                material.update({i[1]: material.get(i[1],0) + mul * i[0]})
-        print(f"A1: {material.get('ORE', 0)}")
+
+        print(f"A1: {fuel(1)}")
 
     # Part 2
-
-    print(f"A2: {0}")
-
+    cargo = 1000000000000
+    lim = [cargo // fuel(1),]
+    lim += [2*lim[0]]
+    while lim[1] - lim[0] > 1:
+        _l = sum(lim) // 2
+        if fuel(_l) > cargo:
+            lim[1] = _l
+        else:
+            lim[0] = _l
+    print(f"A2: {lim[0]}")
 
 if __name__ == '__main__':
     main()
