59b307d446123b29c89f01d506d9973c32b728c2
DavidGrgic <david.grgic@gmail.com>
2022-12-17 21:52:06 +0100

    2022-17

--- 2022/16/naloga16.py
+++ 2022/16/naloga16.py
@@ -6,7 +6,7 @@ import math, copy, os, sys
 import pandas as pd, numpy as np
 #from collections import Counter
 #from fractions import Fraction
-#from itertools import permutations, combinations, product
+from itertools import permutations, combinations, product
 #import networkx as nx   # G = nx.DiGraph(); G.add_edges_from([('Start', 'B'), ('B', 'C'), ('Start', 'C'), ('C', 'End')]); nx.shortest_path(G, 'Start', 'End')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
 _img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]));
@@ -28,9 +28,8 @@ def main():
             m = da[1].replace(' tunnels lead to valves ', '').replace(' tunnel leads to valve ', '').split(', ')
             data.update({k: (int(kn[1]), set(m))})
 
-    tt = 30
 
-    def poti(obiskani, odprti = set(), t0 = 0):
+    def poti(obiskani, odprti = set(), t0 = 0, data = data):
         vv = obiskani[-1]
         pot = set()
         for i, d in dat[vv][1].items():# - set(obiskani[:-2]):
@@ -39,35 +38,57 @@ def main():
                     continue   # Nisma smisla iti k ventilu, ga ne odpreti in se vrniti nazaj
                 _t = t0 + odpri
                 izpust = odpri * dat[vv][0] * max((tt - _t),0)
-                if _t+d >= tt-1 or set(obiskani) == set(dat):
+                if _t+d >= tt-1 or odprti | ({vv} if odpri else set()) == set(dat)-{start}:
                     pot |= {((vv, odpri, izpust, _t),)}
                 else:
                     po = poti(obiskani + [i], odprti | ({vv} if odpri else set()), _t+d)
                     pot |= {((vv, odpri, izpust, _t),) + i for i in po}
         return set((next(iter(sorted(pot, key = lambda x: sum(i[2] for i in x), reverse = True))),)) if len(pot) > 0 else pot
 
-    def comp(vv):
+    def comp(vv, da):
         res = {}
-        for v in data[vv[-1]][1] - set(vv):
-            if data[v][0] == 0:
-                dis = comp(vv + [v])
+        for v in da[vv[-1]][1] - set(vv):
+            if da[v][0] == 0:
+                dis = comp(vv + [v], da)
                 res |= {k: d+1 for k, d in dis.items()}
             else:
                 res.update({v: 1})
         return res
 
     start = 'AA'
-    dat = {}
-    for k in {k for k, v in data.items() if v[0] != 0} | {start}:
-        dis = comp([k])
-        dat.update({k: (data[k][0], dis)})
     # Part 1
-    if True:
+    tt = 30
+    if False:
+        dat = {}
+        for k in {k for k, v in data.items() if v[0] != 0} | {start}:
+            dis = comp([k], data)
+            dat.update({k: (data[k][0], dis)})
         p1 = poti([(start)])
         print(f"A1: {max([sum(i[2] for i in p) for p in p1])}")
 
     # Part 2
-    print(f"A2: {0}")
+    tt = 26
+    ventili_izp = {k: v[0] for k, v in data.items() if v[0] != 0}
+    ventili = ventili_izp.keys()
+    komb = set()
+    for k in product(range(2), repeat = len(ventili)):
+        komb |= {tuple(sorted(tuple(v for i, v in zip(k, ventili) if i == r) for r in range(2)))}
+    komb = sorted(komb, key = lambda x: (abs(len(x[0])-len(x[1])), abs(sum(ventili_izp[i] for i in x[0]) - sum(ventili_izp[i] for i in x[1]))))
+    max_izp = 0
+    for ko in komb:
+        re = (); izp = 0
+        for r in range(2):
+            da = {k: v if k in ko[r] else (0,v[1])  for k, v in data.items()}
+            dat = {}
+            for k in {k for k, v in da.items() if v[0] != 0} | {start}:
+                dis = comp([k], da)
+                dat.update({k: (data[k][0], dis)})
+            po = poti([(start)], data = da)
+            izp += max([sum(i[2] for i in p) for p in po]) if len(po) > 0 else 0
+            re += (ko[r],)
+        max_izp = max(max_izp, izp)
+        print(re, izp, max_izp)
+    print(f"A2: {max_izp}")
 
 if __name__ == '__main__':
     main()
---
+++ 2022/17/naloga17.py
@@ -0,0 +1,101 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import math, copy, os, sys
+import pandas as pd, numpy as np
+#from collections import Counter
+#from fractions import Fraction
+#from itertools import permutations, combinations, product
+#import networkx as nx   # G = nx.DiGraph(); G.add_edges_from([('Start', 'B'), ('B', 'C'), ('Start', 'C'), ('C', 'End')]); nx.shortest_path(G, 'Start', 'End')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+_img_map = {0: '.', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x[::-1]]));
+def _dict2img(x):
+    offset = tuple(int(min(i[j] for i in x.keys())) for j in range(2))
+    img = np.zeros(tuple(int(max(i[j] for i in x.keys())-offset[j])+1 for j in range(2))).astype(int)
+    img[tuple(tuple(int(i[j]-offset[j]) for i in x.keys()) for j in range(2))] = list(x.values())
+    return img
+
+def main():
+    # Read
+    data = []
+    with open('d.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            data = ln.replace('\n', '')
+
+    shape = [((0,0), (0,1), (0,2), (0,3)),
+             ((1,0), (0,1), (1,1), (2,1), (1,2)),
+             ((0,0), (0,1), (0,2), (1,2), (2,2)),
+             ((0,0), (1,0), (2,0), (3,0)),
+             ((0,0), (0,1), (1,0), (1,1))]
+
+    def principal_period(s):
+        i = (s+s).find(s, 1, -1)
+        return None if i == -1 else s[:i]
+
+    def padaj(cik):
+        move = lambda x, m: tuple((i[0]+m[0], i[1]+m[1])for i in x)
+        zadel = lambda x: any(i in sklad for i in x)
+        
+        sklad = []; visina = -1; diff = ''; bias = None
+        k = 0
+        m = 0
+        for r in range(10**10 if cik is None else cik):
+            padajoca = move(shape[k % 5], (visina + 4, 2))
+            while True:
+                _padajoca = move(padajoca, (0, 1 if data[m % len(data)] == '>' else -1))
+                if not zadel(_padajoca) and all(0 <= i[1] <= 6 for i in _padajoca):
+                    padajoca = _padajoca
+                _padajoca = move(padajoca, (-1, 0))
+                m += 1
+                if zadel(_padajoca) or any(i[0] < 0 for i in _padajoca):
+                    break
+                else:
+                    padajoca = _padajoca
+            sklad += list(padajoca)
+            _visina = max(i[0] for i in sklad)
+            diff += str(_visina - visina)
+            visina = _visina
+            sklad = sklad[-128:]
+            k += 1
+            if cik is None:
+                if False and len({i[1] for i in sklad if i[0] >= visina - 1}) == 7:
+                    _img_print(_dict2img({k:1 for k in sklad}))
+                    continue
+                    # tukaj najdemo vzorec, ko so zgornje dve vrstici zapolnjnei, eden od njih je
+                    #
+                    #     .#.....
+                    #     #######
+                    #
+                    # kar ustreza [(visina, 1), (visina-1,0), (visina-1,1), (visina-1,2), (visina-1,3), (visina-1,4), (visina-1,5), (visina-1,6)]
+                if all(i in sklad for i in [(visina, 1), (visina-1,0), (visina-1,1), (visina-1,2), (visina-1,3), (visina-1,4), (visina-1,5), (visina-1,6)]) and bias is None:
+                    bias = r
+                    diff_bias = diff
+                    diff = ''
+                    _img_print(_dict2img({k:1 for k in sklad}))
+                    print(f"bias: {bias}")
+                if bias is not None and len(diff) > 50:
+                    per = principal_period(diff)
+                    if per is not None:
+                        cikel = 10**12
+                        bias_cik = len(diff_bias)
+                        vis = sum(int(i) for i in diff_bias)
+                        cikel -= bias_cik
+                        freq_cik = len(per)
+                        vis += (cikel // freq_cik) * sum(int(i) for i in per)
+                        osta_cik = cikel % freq_cik
+                        vis += sum(int(i) for i in per[:osta_cik])
+                        return vis
+        return visina
+
+    # Part 1
+    if True:
+        p1 = padaj(2022)
+        print(f"A1: {p1+1}")
+
+    # Part 2
+    p2 = padaj(None)
+    print(f"A2: {p2}")
+
+if __name__ == '__main__':
+    main()
