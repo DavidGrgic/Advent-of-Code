32289b00a0a8b622d304bbaf7958f831ff194e03
david_grgic <david.grgic@gen-i.si>
2023-12-01 14:28:07 +0100

    2023-01a

--- 2023/01/naloga01.py
+++ 2023/01/naloga01.py
@@ -23,56 +23,35 @@ def main():
     with open('d.txt', 'r') as file:
         for c, ln in enumerate(file):
             ln = ln.replace('\n', '')
-            # da = [i for i in ln]
-            # data += [da]
             data += [ln]
 
     # Part 1
-    if False:
+    def find(data):
+        for col in data:
+            if col.isdigit():
+                return col
+
+    if True:
         p1 = []
         for row in data:
-            p = []
-            for col in row:
-                if col.isdigit():
-                    p += [col]
-                    break
-            for col in row[::-1]:
-                if col.isdigit():
-                    p += [col]
-                    break
-            p1 += [int(''.join(p))]
+            p1.append(int(''.join((find(row), find(row[::-1])))))
         print(f"A1: {sum(p1)}")
 
-    def repl(row, last = False):
+    # Part 2
+    def str2num(row, last = False):
         res = []
-        if last:
-            row = row[::-1]
         for k, v in rep.items():
             res += [row.replace(k, str(v))]
         if last:
             res = [r[::-1] for r in res]
-        res = sorted(res, key = lambda x: ''.join([str(1 if i.isdigit() else 0) for i in x]).find('1') if '1' in ''.join([str(1 if i.isdigit() else 0) for i in x]) else 10**5)[0]
+        res = sorted(res, key = lambda x: (lambda p = ''.join([str(1 if i.isdigit() else 0) for i in x]).find('1'): p if p >= 0 else 10**6)())[0]
         return res
 
-    # Part 2
     rep = {'one': 1,'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-    p1 = []
+    p2 = []
     for row in data:
-        # for k, v in rep.items():
-        #     row = row.replace(k, str(v))
-        p = []
-        rrr = repl(row)
-        for col in rrr:
-            if col.isdigit():
-                p += [col]
-                break
-        rrr = repl(row[::-1], True)
-        for col in rrr:
-            if col.isdigit():
-                p += [col]
-                break
-        p1 += [int(''.join(p))]
-    print(f"A1: {sum(p1)}")
+        p2.append(int(''.join((find(str2num(row)), find(str2num(row, True))))))
+    print(f"A2: {sum(p2)}")
 
 if __name__ == '__main__':
     main()
---
+++ 2023/score.py
@@ -0,0 +1,60 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd
+import os, json
+
+def main():
+    _json = os.path.join(os.path.dirname(__file__), 'score.json')
+    if not os.path.isfile(_json):
+        return
+    with open(_json) as f:
+        sco = json.load(f)
+    
+    score = []
+    for user_id, data in sco.get('members').items():
+        name = data.get('name')
+        score += (lambda N = name, D = data: [(int(user_id), N, int(d), int(p), int(w.get('get_star_ts'))) for d, v in D.get('completion_day_level').items() for p, w in v.items()])()
+    score = pd.DataFrame(score, columns = ['ID', 'Name', 'Day', 'Part', 'Time'])
+    score['UTC'] = score['Time'].astype('datetime64[s]')
+    score['UTC0'] = [pd.Timestamp(f"{sco['event']}-12-01") + pd.Timedelta(days = v['Day']-1) + pd.Timedelta(hours = 5) for i, v in score.iterrows()]
+    score['Minutes'] = (score['UTC'] - score['UTC0']).astype('timedelta64[s]')/60
+    score['LT'] = pd.DatetimeIndex(score['UTC']).tz_localize('UTC').tz_convert('CET').tz_localize(None)
+    score['LTwkday'] = score['LT'].dt.dayofweek  # 0: Mon ... 6: Sun
+    score['LThour'] = score['LT'].dt.hour
+    
+
+    score_id = score.groupby('ID')['Name'].first()
+
+    # Day/Part statistic
+    minutes = score.loc[(score['Day'] == 1) & (score['Part'] == 2), ['Name', 'Day', 'LT', 'Minutes']].sort_values('Minutes')
+
+    # Name place
+    rank = (lambda N = 'David Grgic', S = score, D = pd.Int64Index([i+1 for i in range(score['Day'].max())], name = 'Day'), P = pd.Int64Index([i for i in range(1,3)], name = 'Part'), np = pd.np: pd.DataFrame([[np.append(np.where(S.loc[(S['Day'] == d) & (S['Part'] == p)].sort_values('Minutes')['Name'].values == N)[0], -1)[0]+1 for p in P] for d in D], index = D, columns = P).astype(str).replace({'0':''}))()
+
+    # Name statistic 
+    names = score.loc[score['Name'] == 'Blaž Peterlin'].sort_values(['Day', 'Part'])
+    
+    # AggregateByNames statistic
+    score.loc[score['Part'] == 2].groupby('ID')[['Minutes']].mean().sort_values(['Minutes']).join(score_id)
+    
+    # Top listaper Day/Part
+    top_no = 5
+    top_lista = None
+    for d in score['Day'].unique():
+        tmp = pd.Series((lambda S = score, d=d, T = top_no: {p: '; '.join('{0} [{1}]'.format(v['Name'], round(v['Minutes'],1)) for i, v in S.loc[(S['Day'] == d) & (S['Part'] == p)].sort_values('Minutes').iloc[:T].iterrows()) for p in range(1,3)})()).to_frame(d).T
+        top_lista = pd.concat([top_lista, tmp])
+    top_lista.index.name = 'Day'
+    top_lista.columns.name = 'Part'
+    top_lista = top_lista.sort_index(axis = 0).sort_index(axis = 1)
+
+    K = score['LTwkday'] <= 4
+    h_wk= score.loc[K].groupby(['LThour', 'Part'])['ID'].count().unstack('Part')
+    h_wkend= score.loc[~K].groupby(['LThour', 'Part'])['ID'].count().unstack('Part')
+    h_week = pd.concat({'Mo-Fr': (100* h_wk / h_wk.sum()).round(1), 'Sa-Su': (100* h_wkend / h_wkend.sum()).round(1)}, axis = 1, names = ['Wkday'])
+    h_week = pd.DataFrame(index = pd.Int64Index(range(24), name = h_week.index.name), columns = pd.MultiIndex.from_tuples([], names = h_week.columns.names)).join(h_week)
+
+if __name__ == '__main__':
+    main()
+#   http://genisidt658:5008/
