402f4edf5cb02ba7843c2baeb89f038d5f83907e
DavidGrgic <david.grgic@gmail.com>
2023-12-22 07:01:34 +0100

    2023-22

--- 2023/22/naloga22.py
+++ 2023/22/naloga22.py
@@ -19,18 +19,44 @@ def _dict2img(x):
 
 def main():
     # Read
-    data = []
+    data = {}
     with open('t.txt', 'r') as file:
         for c, ln in enumerate(file):
             ln = ln.replace('\n', '')
-            if ln == '': # Nov blok podatkov
-                pass
-            da = ln.split(',')
-            data += [da]
+            da = ln.split('~')
+            data.update({c+1: [tuple(int(j) for j in i.split(',')) for i in da]})
 
     # Part 1
     if True:
-        dat=copy.deepcopy(data)
+        dat = np.zeros(tuple(max(i[d] for v in data.values() for i in v)+1 for d in range(3)), dtype = int)  # (z, y, x)
+        for idx, brick in data.items():
+            for x in range(brick[0][0], brick[1][0]+1):
+                for y in range(brick[0][1], brick[1][1]+1):
+                    for z in range(brick[0][2], brick[1][2]+1):
+                        dat[x,y,z] = idx
+        while True:
+            dat_ = dat.copy()
+            # bri = {i: min(np.where(dat_ == i)[2]) for i in data.keys()}
+            # bri = [i[0] for i in sorted(bri.items(), key = lambda x: x[1])]
+            z = 1
+            while z < dat_.shape[0]:
+                plast = dat_[:,:,z]
+                bri = set(i for j in plast for i in j) - {0}
+                premik = False
+                for br in bri:
+                    xy = np.where(plast == br)
+                    if (dat_[xy+(z-1,)] == 0).all():
+                        xyz = np.where(dat_ == br)
+                        dat_[xyz] = 0
+                        dat_[tuple(d-1 if i ==2 else d for i,d in enumerate(xyz))] = br
+                        premik = True
+                if premik:
+                    if z > 1:
+                        z -= 1
+                else:
+                    z += 1
+            if (dat == dat_).all():
+                break
         print(f"A1: {0}")
 
     # Part 2
