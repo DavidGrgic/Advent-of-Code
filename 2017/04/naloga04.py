40983fb4e9b2920bfd170505038ad351ced4d808
David Grgic <david.grgic@gmail.com>
2024-11-26 02:20:56 +0100

    2017-05

---
+++ 2017/04/naloga04.py
@@ -0,0 +1,44 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import math, copy, os, sys
+import pandas as pd, numpy as np
+#from collections import Counter
+#from fractions import Fraction
+#from itertools import permutations, combinations, product
+#from functools import cache   # @cache
+#import networkx as nx   # G = nx.DiGraph(); G.add_edges_from([('Start', 'B'), ('B', 'C'), ('Start', 'C'), ('C', 'End')]); nx.shortest_path(G, 'Start', 'End'); G.add_weighted_edges_from([('Start', 'B', 1.7), ('B', 'C', 0.6), ('Start', 'C', 2.9), ('C', 'End', 0.2)]); nx.shortest_path(G, 'Start', 'End', 'weight')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+_img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]));
+def _dict2img(x):
+    offset = tuple(int(min(i[j] for i in x.keys())) for j in range(2))
+    img = np.zeros(tuple(int(max(i[j] for i in x.keys())-offset[j])+1 for j in range(2))).astype(int)
+    img[tuple(tuple(int(i[j]-offset[j]) for i in x.keys()) for j in range(2))] = list(x.values())
+    return img
+
+def main():
+    # Read
+    data = []
+    with open('d.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            da = ln.split()
+            data += [da]
+
+    # Part 1
+    if True:
+        dat=copy.deepcopy(data)
+        part1 = [len(i) == len(set(i)) for i in dat]
+        print(f"A1: {sum(part1)}")
+
+    # Part 2
+    part2 = []
+    dat=copy.deepcopy(data)
+    for i in dat:
+        i_ = [''.join(sorted(j)) for j in i]
+        part2.append(len(i_) == len(set(i_)))
+    print(f"A2: {sum(part2)}")
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/05/naloga05.py
@@ -0,0 +1,52 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import math, copy, os, sys
+import pandas as pd, numpy as np
+#from collections import Counter
+#from fractions import Fraction
+#from itertools import permutations, combinations, product
+#from functools import cache   # @cache
+#import networkx as nx   # G = nx.DiGraph(); G.add_edges_from([('Start', 'B'), ('B', 'C'), ('Start', 'C'), ('C', 'End')]); nx.shortest_path(G, 'Start', 'End'); G.add_weighted_edges_from([('Start', 'B', 1.7), ('B', 'C', 0.6), ('Start', 'C', 2.9), ('C', 'End', 0.2)]); nx.shortest_path(G, 'Start', 'End', 'weight')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+_img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]));
+def _dict2img(x):
+    offset = tuple(int(min(i[j] for i in x.keys())) for j in range(2))
+    img = np.zeros(tuple(int(max(i[j] for i in x.keys())-offset[j])+1 for j in range(2))).astype(int)
+    img[tuple(tuple(int(i[j]-offset[j]) for i in x.keys()) for j in range(2))] = list(x.values())
+    return img
+
+def main():
+    # Read
+    data = []
+    with open('d.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            data += [int(ln)]
+
+    # Part 1
+    if True:
+        dat=copy.deepcopy(data)
+        count = 0
+        pos = 0
+        while 0 <= pos < len(dat):
+            count += 1
+            jump = dat[pos]
+            dat[pos] += 1
+            pos += jump
+        print(f"A1: {count}")
+
+    # Part 2
+    dat=copy.deepcopy(data)
+    count = 0
+    pos = 0
+    while 0 <= pos < len(dat):
+        count += 1
+        jump = dat[pos]
+        dat[pos] += 1 if jump < 3 else -1
+        pos += jump
+    print(f"A2: {count}")
+
+if __name__ == '__main__':
+    main()
