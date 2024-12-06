220f7e9156205350a778e84bdd9475280af76d0b
David Grgic <david.grgic@gmail.com>
2024-12-06 21:31:17 +0100

    2024-06

---
+++ 2019/23/naloga23.py
@@ -0,0 +1,102 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import pandas as pd, numpy as np
+from collections import Counter
+from fractions import Fraction
+from itertools import permutations, combinations, product
+import os, sys, copy, math
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+import mat
+_img_map = {0: '.', 1: '#'}; _img_print = lambda x: print('\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]))
+
+def main():
+
+    # Read
+    with open('d.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            data = {i: int(v) for i, v in enumerate(ln.split(','))}
+
+    def intcode(dat, _input = [], pos = 0, base = 0, no_out = None):
+        _output = []
+        _consumed = []
+        address = lambda p, m: p+base if m == 2 else p
+        value = lambda p, m: dat.get(address(dat[p],m), 0) if m in {0,2} else dat.get(p, 0)
+        while True:
+            ins = str(dat[pos])
+            ins = (5-len(ins))*'0' + ins
+            mod = [int(i) for i in ins[-3::-1]]
+            ins = int(ins[-2:])
+            if ins == 99:
+                stop = True
+                break
+            if ins == 1:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = value(pos+1, mod[0]) + value(pos+2, mod[1])
+                pos += 4
+            elif ins == 2:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = value(pos+1, mod[0]) * value(pos+2, mod[1])
+                pos += 4
+            elif ins == 3:
+                assert mod[0] in {0,2}
+                assert len(_input) > 0
+                dat[address(dat[pos+1], mod[0])] = _input[0]
+                _consumed += [_input[0]]
+                _input = _input[1:]
+                pos += 2
+            elif ins == 4:
+                _output.append(value(pos+1, mod[0]))
+                pos += 2
+                if no_out is not None and len(_output) >= no_out:
+                    stop = False
+                    break
+            elif ins == 5:
+                if value(pos+1, mod[0]) != 0:
+                    pos = value(pos+2, mod[1])
+                else:
+                    pos += 3
+            elif ins == 6:
+                if value(pos+1, mod[0]) == 0:
+                    pos = value(pos+2, mod[1])
+                else:
+                    pos += 3
+            elif ins == 7:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = 1 if value(pos+1, mod[0]) < value(pos+2, mod[1]) else 0
+                pos += 4
+            elif ins == 8:
+                assert mod[2] in {0,2}
+                dat[address(dat[pos+3], mod[2])] = 1 if value(pos+1, mod[0]) == value(pos+2, mod[1]) else 0
+                pos += 4
+            elif ins == 9:
+                base += value(pos+1, mod[0])
+                pos += 2
+            else:
+                raise AssertionError
+        return dat, _output, -1 if stop else pos, base, _consumed
+
+    # Part 1
+    if True:
+        vhod = {k: [k, -1] for k in range(50)}
+        izhod = {}
+        no = {}
+        for k, i in vhod.items():
+            j = 0
+            while True:
+                try:
+                    _dat, out, _pos, _bas, _con = intcode(copy.copy(data), i, no_out = j)
+                    izhod.update({k: out})
+                    j += 1
+                except:
+                    no.update({k: j-1})
+                    break
+        print(f"A1: {0}")
+
+    # Part 2
+    print(f"A2: {0}")
+
+if __name__ == '__main__':
+    main()
---
+++ 2024/06/naloga06.py
@@ -0,0 +1,67 @@
+﻿# -*- coding: utf-8 -*-
+"""
+@author: David Grgic
+"""
+import math, copy, os, sys
+import pandas as pd, numpy as np
+#from collections import Counter
+#from fractions import Fraction
+#from itertools import permutations, combinations, product
+#from functools import cache   # @cache
+#import networkx as nx   # G = nx.DiGraph(); G.add_edges_from([('Start', 'B'), ('B', 'C'), ('Start', 'C'), ('C', 'End')]); nx.shortest_path(G, 'Start', 'End'); G.add_weighted_edges_from([('Start', 'B', 1.7), ('B', 'C', 0.6), ('Start', 'C', 2.9), ('C', 'End', 0.2)]); nx.shortest_path(G, 'Start', 'End', 'weight')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+_img_map = {0: ' ', 1: '#'}; _img_print = lambda x: print('\n'+'\n'.join([''.join(_img_map.get(i,'?') for i in j) for j in x]));
+def _dict2img(x):
+    offset = tuple(int(min(i[j] for i in x.keys())) for j in range(2))
+    img = np.zeros(tuple(int(max(i[j] for i in x.keys())-offset[j])+1 for j in range(2))).astype(int)
+    img[tuple(tuple(int(i[j]-offset[j]) for i in x.keys()) for j in range(2))] = list(x.values())
+    return img
+
+def main():
+    # Read
+    data = []
+    with open('d.txt', 'r') as file:
+        for c, ln in enumerate(file):
+            ln = ln.replace('\n', '')
+            da = [i == '#' for i in ln]
+            data += [da]
+            p = ln.find('^')
+            if p >= 0:
+                start = (c, p)
+    data = np.array(data)
+    dim_x, dim_y = data.shape
+    ovire = {(int(x),int(y)) for x,y in zip(*np.where(data))}
+    prosti = {(i, j) for i in range(dim_x) for j in range(dim_y)} - ovire - {start}
+    
+    plus = lambda xy, di: (xy[0]+di[0], xy[1]+di[1])
+    turn = {0:(-1,0), 1:(0,1), 2:(1,0), 3:(0,-1)}
+    
+    def pot(xyd, ovi):
+
+        def korak(xyd):
+            xy = plus(xyd[:2], turn[xyd[-1]])
+            return korak(xyd[:2] + ((xyd[-1] + 1) % 4,)) if xy in ovi else xy + (xyd[-1],)
+
+        pot_ = {xyd}
+        while True:
+            xyd = korak(xyd)
+            if xyd in pot_:  # Loop
+                return
+            if xyd[0] >= dim_x or xyd[1] >= dim_y or xyd[0] < 0 or xyd[1] < 0:
+                return pot_
+            pot_ |= {xyd}
+
+    # Part 1
+    if True:
+        p1 = pot(start + (0,), ovire)
+        print(f"A1: {len({i[:2] for i in p1})}")
+
+    # Part 2
+    p2 = set()
+    for dod in prosti:
+        if pot(start + (0,), ovire | {dod}) is None:
+            p2 |= {dod}
+    print(f"A2: {len(p2)}")
+
+if __name__ == '__main__':
+    main()
