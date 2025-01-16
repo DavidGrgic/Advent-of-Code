33a61912a8e77448b13371c54ad811c6eaffe94a
David Grgic <david.grgic@gmail.com>
2025-01-16 22:38:56 +0100

    2018-09

--- 2018/09/naloga09.py
+++ 2018/09/naloga09.py
@@ -22,23 +22,39 @@ def plot(data, mapper: dict = {0: '.', 1: '#'}, default: dict = {set: 1, dict: 0
 
 def main():
     # Read
-    data = []
-    with open('t.txt', 'r') as file:
-        for c, ln in enumerate(file):
-            ln = ln.replace('\n', '')
-            if ln == '': # Nov blok podatkov
-                pass
-            da = ln.split(',')
-            data += [da]
+    players = 404; marbles = 71852
+    #players = 10; marbles = 1618
+    #players = 9; marbles = 25
+
+    def game(players, marbles):
+        player = {}
+        seq = {0: [0,0]}
+        current = 0
+        for marble in range(1, marbles+1):
+            if marble % 23 == 0:
+                for _ in range(8):
+                    current, nxt = seq[current_ := current]
+                seq[current][1] = nxt
+                seq[nxt][0] = current
+                current = nxt
+                del seq[current_]
+                ply = ((marble - 1) % players) + 1
+                player.update({ply: player.get(ply, set()) | {marble, current_}})
+            else:
+                _, current = seq[prv := current]
+                _, nxt = seq[current]
+                seq.update({current: [prv if prv != current else marble, marble], marble: [current, nxt], nxt: [marble, seq[nxt][1] if nxt != current else marble]})
+                current = marble
+        return {k: sum(v) for k, v in player.items()}
 
     # Part 1
     if True:
-        dat=copy.deepcopy(data)
-        print(f"A1: {0}")
+        p1 = game(players, marbles)
+        print(f"A1: {max(p1.values())}")
 
     # Part 2
-    dat=copy.deepcopy(data)
-    print(f"A2: {0}")
+    p2 = game(players, 100*marbles)
+    print(f"A2: {max(p2.values())}")
 
 if __name__ == '__main__':
     main()
